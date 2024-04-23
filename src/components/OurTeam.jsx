f164bae480a78746b49969eebfe037363d076a11
sachin-niyana <sachinjakhar100@gmail.com>
2024-04-23 17:04:28 +0530

    marge code

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -13,24 +13,24 @@ const OurTeam = () => {
           Our team
         </h2>
         <div className="flex lg:flex-row flex-col justify-center gap-8 items-center">
-          <div className=" bg-white rounded-xl py-12 px-9 max-w-[704px] shadow-[0_0px_12px_0px_rgba(0,0,0,0.07)]">
-            <div className="flex justify-start gap-6 items-center">
+          <div className=" bg-white rounded-xl sm:py-12 px-4 sm:px-9 max-w-[704px] shadow-[0_0px_12px_0px_rgba(0,0,0,0.07)]">
+            <div className="flex sm:flex-row flex-col justify-start gap-6 items-center">
               <img className="w-[108px] h-[108px]" src={julian} alt="julian" />
               <div className="flex flex-col">
                 <p className="font-poppins font-semibold text-xl text-darkblue">
                   Julian Elysian
                 </p>
-                <p className="font-poppins font-normal text-md text-darkblue">
+                <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue">
                   Co-Founder & CEO
                 </p>
               </div>
             </div>
-            <div className="flex justify-start gap-[45px] items-start mt-1.5">
-              <div className="flex justify-start gap-[18px] mt-[18px] items-center">
+            <div className="flex sm:flex-row flex-col-reverse justify-start sm:gap-[45px] gap-8 items-start mt-1.5">
+              <div className="flex justify-start gap-[18px] sm:mt-[18px] items-center mx-auto sm:ms-[21px]">
                 <Linkedin />
                 <Twitter />
               </div>
-              <p className="font-poppins font-normal text-md text-darkblue leading-[144%]">
+              <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue leading-[144%]">
                 There are many variations of passages of Lorem is a Ipsum
                 available, but the majority have suffered the alteration in some
                 form, by injected humour, or let randomised words which but the
@@ -39,24 +39,24 @@ const OurTeam = () => {
               </p>
             </div>
           </div>
-          <div className=" bg-white rounded-xl py-12 px-9 max-w-[704px] shadow-[0_0px_12px_0px_rgba(0,0,0,0.07)]">
-            <div className="flex justify-start gap-6 items-center">
+          <div className=" bg-white rounded-xl sm:py-12 py-8 px-4 sm:px-9 max-w-[704px] shadow-[0_0px_12px_0px_rgba(0,0,0,0.07)]">
+            <div className="flex sm:flex-row flex-col justify-start gap-6 items-center">
               <img className="w-[108px] h-[108px]" src={alisa} alt="alisa" />
               <div className="flex flex-col">
                 <p className="font-poppins font-semibold text-xl text-darkblue">
                   Alisa Elysian
                 </p>
-                <p className="font-poppins font-normal text-md text-darkblue">
+                <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue">
                   Co-Founder & CEO
                 </p>
               </div>
             </div>
-            <div className="flex justify-start gap-[45px] items-start mt-1.5">
-              <div className="flex justify-start gap-[18px] mt-[18px] items-center">
+            <div className="flex sm:flex-row flex-col-reverse justify-start sm:gap-[45px] gap-8 items-start mt-1.5">
+              <div className="flex justify-start gap-[18px] sm:mt-[18px] items-center mx-auto sm:ms-[21px]">
                 <Linkedin />
                 <Twitter />
               </div>
-              <p className="font-poppins font-normal text-md text-darkblue leading-[144%]">
+              <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue leading-[144%]">
                 There are many variations of passages of Lorem is a Ipsum
                 available, but the majority have suffered the alteration in some
                 form, by injected humour, or let randomised words which but the
