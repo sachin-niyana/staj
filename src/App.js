156b950292f64cba353c3db532eff171c2679e6a
sachin-niyana <sachinjakhar100@gmail.com>
2024-04-23 14:08:23 +0530

    marge code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import OurTeam from "./components/OurTeam";
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <OurTeam />
     </>
   );
 }
---
+++ src/components/OurTeam.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OurTeam = () => {
+  return (
+    <div className="bg-white">
+      <div className="container max-w-[1164px] mx-auto px-3 pt-[120px]">
+        <p>Team</p>
+      </div>
+    </div>
+  );
+};
+
+export default OurTeam;
