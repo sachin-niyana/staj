8e772d5dd8a66ee513dc1246117d18460544e6a2
mayankpcp7 <jariwalmayank123@gmail.com>
2024-04-23 14:14:22 +0530

    merge code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import mobile from "../assets/images/webp/mobiles.webp";
 const Header = () => {
     return (
         <>
-            <div className="bg-grey">
+            <div className="bg-skyblue">
                 <div className="max-w-[1464px] px-3 mx-auto">
                     <div className="flex lg:justify-between mt-[72px]">
                         <div className="flex flex-col mt-[153px]">
@@ -12,7 +12,7 @@ const Header = () => {
                                 <span className="text-golden"> support local artists</span>
                             </h1>
                             <p className="ff-poppins text-dakblue fs-md max-w-[570px] pt-[18px]">A brand new way to explore the arts in your city. Currently in beta, exclusively available for iPhone users - download and get started today!</p>
-                            <button className="bg-golden block font-poppins text-base h-[64px] hover:bg-darkblue hover:text-white duration-300 w-[212px] font-bold capitalize py-5 px-10 rounded-[30px] mt-[] text-darkblue">
+                            <button className="bg-golden block font-poppins text-base h-[64px] hover:bg-darkblue hover:text-white duration-300 w-[212px] font-bold capitalize py-5 px-10 rounded-[30px] mt-15 text-darkblue">
                                 DOWNLOAD APP
                             </button>
                         </div>
