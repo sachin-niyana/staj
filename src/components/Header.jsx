7201a739b52e14fd40edbc6d36342df77625cff2
mayankpcp7 <jariwalmayank123@gmail.com>
2024-04-23 15:50:06 +0530

    merge code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import mobile from "../assets/images/webp/mobiles.webp";
 import lefttriangle from "../assets/images/png/header_left_vector.png"
+import circles from "../assets/images/png/slider_circles.png"
 const Header = () => {
     return (
         <>
-            <div className="bg-skyblue">
+            <div className="bg-skyblue overflow-x-">
                 <div className="max-w-[1464px] px-3 mx-auto">
                     <div className="flex md:flex-row flex-col-reverse items-center md:items-start md:justify-between pt-[40px] md:pt-[72px]">
-                        <div className="flex flex-col -mt-7 md:mt-[50px] xl:mt-[153px] relative">
+                        <div className="flex flex-col items-center md:items-start -mt-7 md:mt-[50px] xl:mt-[153px] relative">
                             <img className="max-w-[161px] absolute start-[-26%] top-[20%] sm:block hidden" src={lefttriangle} alt="vector" />
-                            <h1 className="ff-poppins font-bold text-[32px] sm:text-[38px] md:text-start text-center lg:text-xxl text-darkblue max-w-[640px] leading-[113%]">
+                            <h1 className="ff-poppins font-bold text-[32px] sm:text-[38px] md:text-start text-center lg:text-[45px] xl:text-xxl text-darkblue md:max-w-[484px] xl:max-w-[640px] leading-[113%]">
                                 Easily find arts events and
                                 <span className="text-golden"> support local artists</span>
                             </h1>
@@ -19,8 +20,14 @@ const Header = () => {
                             </button>
                         </div>
                         <div>
-                            <img className="max-w-[300px] sm:max-w-[360px] lg:max-w-[500px] xl:max-w-[757px]" src={mobile} alt="mobile" />
+                            <img className="max-w-[300px] 2xl:me-[-65px] sm:max-w-[360px] lg:max-w-[500px] xl:max-w-[757px]" src={mobile} alt="mobile" />
                         </div>
+                    </div>
+                </div>
+                <div className="bg-[rgba(250,229,229)] h-[66px] mt-6 lg:-mt-[75px]  z-2">
+                    <div className="bg-orange  z-3 top-0 flex items-center justify-center w-full h-[74px] opacity-100 -rotate-2">
+                        <p className="ff-poppins text-lg text-white font-medium">Automation Your Project</p>
+                        <img className="max-w-[64px]" src={circles} alt="" />
                     </div>
                 </div>
             </div>
