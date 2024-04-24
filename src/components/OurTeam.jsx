c9d49a5f29cf427f90e89307ba578abef49b65c8
sachin-niyana <sachinjakhar100@gmail.com>
2024-04-24 09:30:57 +0530

    marge code

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import julian from "../assets/images/png/julian.png";
 import alisa from "../assets/images/png/alisa.png";
-import { Linkedin, Twitter } from "./common/Icon";
+import { DotedBox, LineBox, Linkedin, Twitter } from "./common/Icon";
 const OurTeam = () => {
   return (
     <div className="bg-white">
@@ -12,57 +12,71 @@ const OurTeam = () => {
         <h2 className="font-poppins font-extrabold text-xxl text-darkblue text-center mt-3">
           Our team
         </h2>
-        <div className="flex lg:flex-row flex-col justify-center gap-8 items-center">
-          <div className=" bg-white rounded-xl sm:py-12 px-4 sm:px-9 max-w-[704px] shadow-[0_0px_12px_0px_rgba(0,0,0,0.07)]">
-            <div className="flex sm:flex-row flex-col justify-start gap-6 items-center">
-              <img className="w-[108px] h-[108px]" src={julian} alt="julian" />
-              <div className="flex flex-col">
-                <p className="font-poppins font-semibold text-xl text-darkblue">
-                  Julian Elysian
-                </p>
-                <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue">
-                  Co-Founder & CEO
-                </p>
-              </div>
+        <div className="flex lg:flex-row flex-col justify-center gap-8 items-center mt-[60px]">
+          <div className="relative">
+            <div className="absolute md:block hidden bottom-[-10%] left-[-8%]">
+              <DotedBox />
             </div>
-            <div className="flex sm:flex-row flex-col-reverse justify-start sm:gap-[45px] gap-8 items-start mt-1.5">
-              <div className="flex justify-start gap-[18px] sm:mt-[18px] items-center mx-auto sm:ms-[21px]">
-                <Linkedin />
-                <Twitter />
+            <div className=" bg-white rounded-xl sm:py-12 py-8 px-4 sm:px-9 max-w-[704px] shadow-[5px_8px_20px_0px_rgba(0,0,0,0.07)] relative">
+              <div className="flex sm:flex-row flex-col justify-start gap-6 items-center">
+                <img
+                  className="w-[108px] h-[108px]"
+                  src={julian}
+                  alt="julian"
+                />
+                <div className="flex flex-col">
+                  <p className="font-poppins font-semibold text-xl text-darkblue">
+                    Julian Elysian
+                  </p>
+                  <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue">
+                    Co-Founder & CEO
+                  </p>
+                </div>
               </div>
-              <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue leading-[144%]">
-                There are many variations of passages of Lorem is a Ipsum
-                available, but the majority have suffered the alteration in some
-                form, by injected humour, or let randomised words which but the
-                majority have be a suffered alteration in some form, by injected
-                humour, don't look even slightly believable.
-              </p>
+              <div className="flex sm:flex-row flex-col-reverse justify-start sm:gap-[45px] gap-8 items-start mt-1.5">
+                <div className="flex justify-start gap-[18px] sm:mt-[18px] items-center mx-auto sm:ms-[21px]">
+                  <Linkedin />
+                  <Twitter />
+                </div>
+                <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue leading-[144%]">
+                  There are many variations of passages of Lorem is a Ipsum
+                  available, but the majority have suffered the alteration in
+                  some form, by injected humour, or let randomised words which
+                  but the majority have be a suffered alteration in some form,
+                  by injected humour, don't look even slightly believable.
+                </p>
+              </div>
             </div>
           </div>
-          <div className=" bg-white rounded-xl sm:py-12 py-8 px-4 sm:px-9 max-w-[704px] shadow-[0_0px_12px_0px_rgba(0,0,0,0.07)]">
-            <div className="flex sm:flex-row flex-col justify-start gap-6 items-center">
-              <img className="w-[108px] h-[108px]" src={alisa} alt="alisa" />
-              <div className="flex flex-col">
-                <p className="font-poppins font-semibold text-xl text-darkblue">
-                  Alisa Elysian
-                </p>
-                <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue">
-                  Co-Founder & CEO
-                </p>
-              </div>
+          <div className="relative">
+            <div className="absolute md:block hidden top-[-31.5%] right-[-11%]">
+              <LineBox />
             </div>
-            <div className="flex sm:flex-row flex-col-reverse justify-start sm:gap-[45px] gap-8 items-start mt-1.5">
-              <div className="flex justify-start gap-[18px] sm:mt-[18px] items-center mx-auto sm:ms-[21px]">
-                <Linkedin />
-                <Twitter />
+            <div className=" bg-white rounded-xl sm:py-12 py-8 px-4 sm:px-9 max-w-[704px] shadow-[-5px_8px_20px_0px_rgba(0,0,0,0.07)] relative">
+              <div className="flex sm:flex-row flex-col justify-start gap-6 items-center">
+                <img className="w-[108px] h-[108px]" src={alisa} alt="alisa" />
+                <div className="flex flex-col">
+                  <p className="font-poppins font-semibold text-xl text-darkblue">
+                    Alisa Elysian
+                  </p>
+                  <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue">
+                    Co-Founder & CEO
+                  </p>
+                </div>
               </div>
-              <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue leading-[144%]">
-                There are many variations of passages of Lorem is a Ipsum
-                available, but the majority have suffered the alteration in some
-                form, by injected humour, or let randomised words which but the
-                majority have be a suffered alteration in some form, by injected
-                humour, don't look even slightly believable.
-              </p>
+              <div className="flex sm:flex-row flex-col-reverse justify-start sm:gap-[45px] gap-8 items-start mt-1.5">
+                <div className="flex justify-start gap-[18px] sm:mt-[18px] items-center mx-auto sm:ms-[21px]">
+                  <Linkedin />
+                  <Twitter />
+                </div>
+                <p className="font-poppins font-normal sm:text-start text-center text-md text-darkblue leading-[144%]">
+                  There are many variations of passages of Lorem is a Ipsum
+                  available, but the majority have suffered the alteration in
+                  some form, by injected humour, or let randomised words which
+                  but the majority have be a suffered alteration in some form,
+                  by injected humour, don't look even slightly believable.
+                </p>
+              </div>
             </div>
           </div>
         </div>
