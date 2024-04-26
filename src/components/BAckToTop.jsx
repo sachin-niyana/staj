a8a53f83873e7c56d1d4622c7380b2abf22f6d1b
mayankpcp7 <jariwalmayank123@gmail.com>
2024-04-26 14:27:59 +0530

    merge code

--- src/components/BAckToTop.jsx
+++ src/components/BAckToTop.jsx
@@ -25,7 +25,7 @@ const BackToTop = () => {
       {isBackToTopVisible && (
         <button
           onClick={scrollToTop}
-          className="fixed sm:bottom-5 bottom-8 right-5 sm:w-[54px] sm:h-[54px] w-[40px] h-[40px] bg-golden animate-bounce text-white flex justify-center items-center rounded-full z-50 transition-all duration-300 "
+          className="fixed sm:bottom-5 bottom-8 hover:bg-darkblue hover:border hover:border-golden right-5 sm:w-[54px] sm:h-[54px] w-[40px] h-[40px] bg-golden animate-bounce text-white flex justify-center items-center rounded-full z-50 transition-all duration-300 "
         >
           <TiArrowUpThick className="sm:w-6 sm:h-6" />
         </button>
